c9c8dd3646f9903b99679d6235be1f090d52fd60
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2021-12-27 16:14:24 -0600

    secured secret key

--- portfolio/pfolio/pfolio/settings.py
+++ portfolio/pfolio/pfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4sr+dhs3c(f#cr)p-zdd$os--f3g*7bg!ww%$!22cja%&cwhv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
