4c24df0184e0c3afecdbba318b811fd34728c0ed
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2021-12-29 17:51:05 -0600

    finished about page

--- portfolio/pfolio/main/urls.py
+++ portfolio/pfolio/main/urls.py
@@ -1,10 +1,9 @@
-from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('resume/', views.resume, name='resume'),
+    path('about/', views.resume, name='about'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
     path('contact_request/', views.contact_request, name='contact_req')
